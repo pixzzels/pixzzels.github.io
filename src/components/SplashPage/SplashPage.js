0367755b55fdf941d2526ad142030de762ea7e0d
Ellen Park <paarkellen@gmail.com>
2021-05-22 23:42:57 -0400

    basic splash page done

--- src/components/SplashPage/SplashPage.js
+++ src/components/SplashPage/SplashPage.js
@@ -4,7 +4,13 @@ import "./SplashPage.css"
 function SplashPage() {
     return (
         <div className="splash-page-container">
-
+            <div className="name-container">
+                <div className="first-name"> &lt; ELLEN &gt;</div>
+                    <p id="mybio">
+                        A little bit about me 
+                    </p>
+                <div className="last-name">&lt; PARK /&gt;</div>
+            </div>
         </div>
     )
 }
