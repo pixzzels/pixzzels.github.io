7f3d8792fd6b8490292bf99b1feb30f793d1b266
Ellen Park <paarkellen@gmail.com>
2021-06-21 15:34:27 -0400

    working on project card

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import { Modal } from '../../context/Modal';
 import Programmerr from '../Projects/Programmerr';
-import ggPlays from './ggPlays.png'
+import programmerrIcon from '../Projects/Programmerr/programmerr-icon.png';
+import luminnoIcon from '../Projects/Luminno/luminno-icon.png';
+
 
 import './Portfolio.css';
 
@@ -15,6 +17,26 @@ function Portfolio() {
     // // if (isRegister) {
     // component = <Programmerr />
 
+    const cardOverlay = (name, link) => {
+        return (
+            <div className="project-card-overlay">
+                <div className="project-card-overlay__header">
+                    {name}
+                </div>
+                <button className="project-card-overlay__learn-more" onClick={() => setOpen(true)}>Click Here to Learn More!!</button>
+                <a className="project-card-overlay__live-link" href={link}>
+                    Live Link
+                </a>
+
+            </div>
+        )
+    }
+
+    // let component =
+    //     <div className="project-card-overlay">
+
+    //     </div>
+
     return (
 
 
@@ -27,20 +49,41 @@ function Portfolio() {
                     <div className="menu-bar menu-bar__other" onClick={() => setContent('all')}>All</div>
                 </div>
                 <div className="projects-container">
+                    {/* <div className="project-card-shadow"></div> */}
+                    {/* <div className="project-card-shadow"></div>
+                    <div className="project-card-shadow"></div> */}
+
+
                     <div className="project-card programmerr">
-                        <button onClick={() => setOpen(true)}>Programmerr</button>
+                        
+                        {cardOverlay("Programmer", "https://programmerr.herokuapp.com/")}
+
+                        <img className="project-card-img" src={programmerrIcon}></img>
+
                         {open && (
                             <Modal className="login-register-modal" onClose={() => setOpen(false)}>
                                 <Programmerr />
                             </Modal>
                         )}
                     </div>
-                    <div className="project-card luminno">Luminno</div>
+                    <div className="project-card luminno">
+                        {cardOverlay("Luminno", "https://programmerr.herokuapp.com/")}
+
+                        <img className="project-card-img" src={luminnoIcon}></img>
+
+                        {open && (
+                            <Modal className="login-register-modal" onClose={() => setOpen(false)}>
+                                <Programmerr />
+                            </Modal>
+                        )}
+                    </div>
                     <div className="project-card chaos-robinhood">Chaos-Robinhood</div>
                     <div className="project-card ggPlays">
                         {/* <img className="project-card-img" src={ggPlays}></img> */}
 
                     </div>
+
+
 
                 </div>
 
