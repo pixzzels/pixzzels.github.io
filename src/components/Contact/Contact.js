740987d5166fe0b86a3b1011b02f77e0012452ad
Ellen Park <paarkellen@gmail.com>
2021-06-29 12:30:57 -0400

    added contact form styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 import './Contact.css'
 
@@ -8,42 +8,70 @@ function Contact() {
     if (state.succeeded) {
         return <p>Your message has been sent!</p>;
     }
+
+    const colors = ["#ff7694", "#2FE6DE", "#F5E0B7", "#8d8de0", "#FF6B6B", "#CBF7ED", "#CA907E", "#FAFAC6"];
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * max);
+    }
+
+    function changeBackground(e) {
+        let number = getRandomInt(5)
+        e.target.style.borderColor = colors[number];
+        e.target.style.color = "white";
+
+    }
+
+    function changeBackground2(e) {
+        e.target.style.borderColor = "#B0B3B8";
+        e.target.style.color = "#B0B3B8";
+    }
+
+
     return (
         <div>
             <form className="contact-form" onSubmit={handleSubmit}>
                 <h2>Get in touch!</h2>
                 <div id="contact-form__name-email-container">
+                    <div className="contact-form__flex">
 
-                    <label className="contact-form__label" htmlFor="name">Name</label>
-                    <input type="text" name="name" id="name" placeholder="Name" />
-                    <label className="contact-form__label" htmlFor="email">
-                        Email Address
+                        <label className="contact-form__label" htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" />
+
+                    </div>
+                    <div className="contact-form__flex" style={{marginLeft:"35px"}}>
+                        <label className="contact-form__label" htmlFor="email">
+                            Email Address
+                        </label>
+                        <input
+                            id="email"
+                            type="email"
+                            name="email"
+                        />
+                        <ValidationError
+                            prefix="Email"
+                            field="email"
+                            errors={state.errors}
+                        />
+                    </div>
+                </div>
+                <div className="contact-form__flex2">
+                    <label className="contact-form__label" htmlFor="message" style={{marginTop:"30px"}}>
+                        Message
                     </label>
-                    <input
-                        id="email"
-                        type="email"
-                        name="email"
+                    <textarea
+                        id="message"
+                        name="message"
+                        placeholder=""
                     />
                     <ValidationError
-                        prefix="Email"
-                        field="email"
+                        prefix="Message"
+                        field="message"
                         errors={state.errors}
                     />
                 </div>
-                <label className="contact-form__label" htmlFor="message">
-                    Message
-                </label>
-                <textarea
-                    id="message"
-                    name="message"
-                />
-                <ValidationError
-                    prefix="Message"
-                    field="message"
-                    errors={state.errors}
-                />
-                <button type="submit" disabled={state.submitting}>
-                    Submit
+                <button className="contact-form-btn" type="submit" disabled={state.submitting} onMouseOver={changeBackground} onMouseLeave={changeBackground2}>
+                    Send
                 </button>
             </form>
         </div>
